c149a5b035c6a22b72e72720b18c6f94ee2f36d2
miyeon396 <miyeon396@naver.com>
2024-04-23 22:52:07 +0900

    ch4. datajpa로 값, dto 반환

---
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -0,0 +1,17 @@
+package study.datajpa.dto;
+
+import lombok.Data;
+
+@Data //엔티티엔 데이터 웬만하면 쓰면 안됨
+public class MemberDto {
+
+    private Long id;
+    private String username;
+    private String teamName;
+
+    public MemberDto(Long id, String username, String teamName) {
+        this.id = id;
+        this.username = username;
+        this.teamName = teamName;
+    }
+}
--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -20,4 +21,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t ") //생성자로 매칭이 되게 일케하면 DTO로 반환
+    List<MemberDto> findMemberDto();
 }
